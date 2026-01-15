87b99338d90c179833dce8cfd2a356dca4c3a346
Odpp7 <Odpp7@users.noreply.github.com>
2026-01-15 14:55:53 -0500

    First commit

---
+++ src/main/main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 900,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, '../preload/preload.js')
+    }
+  });
+
+  win.loadFile(
+    path.join(__dirname, '../renderer/pages/index.html')
+  );
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
